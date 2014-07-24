bae9f86d2b8923cbe211a5bba8ee63e7c071d2fb
Satyajeet Pal <psatyajeet@gmail.com>
2014-07-23 23:59:33 -0700

    finish sign-in

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
   root 'welcome#index'
 
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
 
 
