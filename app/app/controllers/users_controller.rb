25d1e8792baca58598e631af6e71f8f322c1002e
Satyajeet Pal <pal@wealthfront.com>
2014-07-26 17:16:50 -0700

    Users should be done

--- app/app/controllers/users_controller.rb
+++ app/app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:edit, :update]
+	before_action :correct_user,   only: [:edit, :update]
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -18,10 +21,31 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+	  @user = User.find(params[:id])
+	end
+
+	def update
+	end
+
 	private
 
 	def user_params
 		params.require(:user).permit(:name, :email, :password,
 			:password_confirmation)
 	end
+
+	# before filters
+	
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+	def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
